e2b6e89a26f9163bf9a047f676072d29de4ae81a
BRomeo777 <bananeza777@gmail.com>
2026-03-06 15:53:40 +0200

    Update base.py

--- core/models/base.py
+++ core/models/base.py
@@ -1,1 +1,6 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs
 
+class Base(AsyncAttrs, DeclarativeBase):
+    """Base class for all SQLAlchemy asynchronous models."""
+    pass
